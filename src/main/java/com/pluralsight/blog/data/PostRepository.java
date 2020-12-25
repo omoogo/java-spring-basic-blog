ffa820c688d3a9747e35c331dca062364b39e31a
Michael Broniola <mbroniola@gmail.com>
2020-12-25 13:52:14 -0800

    Module 4 updates

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -16,6 +16,12 @@ public class PostRepository {
     }
 
     public Post findById(Long id) {
+        for (Post post : ALL_POSTS) {
+            if (post.getId() == id) {
+                return post;
+            }
+        }
+
         return null;
     }
 
